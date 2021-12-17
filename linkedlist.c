1e0fbc9f3600df5ea6031c10510479dd03ba8727
Hoang Van <77384140+HoangVan20010933@users.noreply.github.com>
2021-12-17 16:59:55 +0700

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,64 +1,62 @@
 #include<stdio.h>
 #include<stdlib.h>
- 
+
 struct LinkedList{
     int data;
     struct LinkedList *next;
  };
- 
-typedef struct LinkedList *node; //Từ giờ dùng kiểu dữ liệu LinkedList có thể thay bằng node cho ngắn gọn
-//tao mot node 
+
+typedef struct LinkedList *node; 
+//tao mot node
 node CreateNode(int value){
-    node temp; // declare a node
-    temp = (node)malloc(sizeof(struct LinkedList)); // Cấp phát vùng nhớ dùng malloc()
-    temp->next = NULL;// Cho next trỏ tới NULL
-    temp->data = value; // Gán giá trị cho Node
-    return temp;//Trả về node mới đã có giá trị
+    node temp; 
+    temp = (node)malloc(sizeof(struct LinkedList)); 
+    temp->next = NULL;
+    temp->data = value;
+    return temp;
 }
 //Them 1 phan tu vao cuoi danh sach
 node AddTail(node head, int value){
-    node temp,p;// Khai báo 2 node tạm temp và p
-    temp = CreateNode(value);//Gọi hàm createNode để khởi tạo node temp có next trỏ tới NULL và giá trị là value
+    node temp,p;
+    temp = CreateNode(value);
     if(head == NULL){
-        head = temp;     //Nếu linked list đang trống thì Node temp là head luôn
+        head = temp;   
     }
     else{
-        p  = head;// Khởi tạo p trỏ tới head
+        p  = head;
         while(p->next != NULL){
-            p = p->next;//Duyệt danh sách liên kết đến cuối. Node cuối là node có next = NULL
+            p = p->next;
         }
-        p->next = temp;//Gán next của thằng cuối = temp. Khi đó temp sẽ là thằng cuối(temp->next = NULL mà)
+        p->next = temp;
     }
     return head;
 }
- 
+
 //them 1 phan tu vao dau ds
 node AddHead(node head, int value){
-    node temp = CreateNode(value); // Khởi tạo node temp với data = value
+    node temp = CreateNode(value); 
     if(head == NULL){
-        head = temp; // //Nếu linked list đang trống thì Node temp là head luôn
+        head = temp; 
     }else{
-        temp->next = head; // Trỏ next của temp = head hiện tại
-        head = temp; // Đổi head hiện tại = temp(Vì temp bây giờ là head mới mà)
+        temp->next = head; 
+        head = temp; 
     }
     return head;
 }
 //Them vao vi tri bat ky
 node AddAt(node head, int value, int position){
     if(position == 0 || head == NULL){
-        head = AddHead(head, value); // Nếu vị trí chèn là 0, tức là thêm vào đầu
+        head = AddHead(head, value);
     }else{
-        // Bắt đầu tìm vị trí cần chèn. Ta sẽ dùng k để đếm cho vị trí
+        
         int k = 1;
         node p = head;
         while(p != NULL && k != position){
             p = p->next;
             ++k;
         }
- 
+
         if(k != position){
-            // Nếu duyệt hết danh sách lk rồi mà vẫn chưa đến vị trí cần chèn, ta sẽ mặc định chèn cuối
-            // Nếu bạn không muốn chèn, hãy thông báo vị trí chèn không hợp lệ
             head = AddTail(head, value);
             // printf("Vi tri chen vuot qua vi tri cuoi cung!\n");
         }else{
@@ -96,33 +94,21 @@ node DelAt(node head, int position){
     if(position == 0 || head == NULL || head->next == NULL){
         head = DelHead(head); // Nếu vị trí chèn là 0, tức là thêm vào đầu
     }else{
-        // Bắt đầu tìm vị trí cần chèn. Ta sẽ dùng k để đếm cho vị trí
         int k = 1;
         node p = head;
         while(p->next->next != NULL && k != position){
             p = p->next;
             ++k;
         }
- 
+
         if(k != position){
-            // Nếu duyệt hết danh sách lk rồi mà vẫn chưa đến vị trí cần chèn, ta sẽ mặc định xóa cuối
-            // Nếu bạn không muốn xóa, hãy thông báo vị trí xóa không hợp lệ
             head = DelTail(head);
-            // printf("Vi tri xoa vuot qua vi tri cuoi cung!\n");
+            printf("Vi tri xoa vuot qua vi tri cuoi cung!\n");
         }else{
             p->next = p->next->next;
         }
     }
     return head;
-}
-//Lay gia tri o vi tri bat ki
-int Get(node head, int index){
-    int k = 0;
-    node p = head;
-    while(p != NULL && k != index){
-        p = p->next;
-    }
-    return p->data;
 }
 //tim kiem trong danh sach
 int Search(node head, int value){
@@ -134,15 +120,6 @@ int Search(node head, int value){
         ++position;
     }
     return -1;
-}
-//Xoa toan bo cac node
-node DelByVal(node head, int value){
-    int position = Search(head, value);
-    while(position != -1){
-        DelAt(head, position);
-        position = Search(head, value);
-    }
-    return head;
 }
 //Duyet dslk
 void Traverser(node head){
@@ -157,14 +134,6 @@ node InitHead(){
     head = NULL;
     return head;
 }
-//Lay so phan tu của dslk
-int Length(node head){
-    int length = 0;
-    for(node p = head; p != NULL; p = p->next){
-        ++length;
-    }
-    return length;
-}
 //Nhap dslk
 node Input(){
     node head = InitHead();
@@ -173,7 +142,7 @@ node Input(){
         printf("\nNhap so luong phan tu trong linked list = ");
         scanf("%d", &n);
     }while(n <= 0);
- 
+
     for(int i = 0; i < n; ++i){
         printf("\nNhap gia tri bien: ");
         scanf("%d", &value);
@@ -181,7 +150,7 @@ node Input(){
     }
     return head;
 }
- 
+
 int main(){
     printf("\nTao 1 danh sach lien ket");
     node head = Input();
@@ -198,27 +167,4 @@ int main(){
     printf("\nThem phan tu 9 vao vi tri 2 linked list");
     head = AddAt(head, 9, 2);
     Traverser(head);
-
-    printf("\nXoa 1 phan dau linked list");
-    head =DelHead(head);
-    Traverser(head);
-
-    printf("\nXoa 1 phan tu bat ki khoi linked list");
-    head = DelAt(head, 1);
-    Traverser(head);
-
-    printf("\nXoa 1 phan tu cuoi linked list");
-    head = DelTail(head);
-    Traverser(head);
- 
-    printf("\nLay 1 pham tu o vi tri thu 3");
-    Get(head,3);
- 
-    printf("\nTim kiem 1 phan tu trong linked list");
-    int index = Search(head, 5);
-    printf("\nTim thay tai chi so %d", index);
-
-    printf("\nXoa toan bo danh sach");
-    head = DelByVal(head,5);
-    Traverser(head);
-}
+                                                           
